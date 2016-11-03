e35595206810a834f01376a923c3768b3e3f9cd9
Emiliano Menendez <emenendez@starmount.com>
2016-11-03 10:58:24 -0300

    Added @JsonIgnore to DayWeather.value in order to avoid showing that field in the HTTP responses.

--- src/main/java/com/mercadolibre/galaxy/weather/DayWeather.java
+++ src/main/java/com/mercadolibre/galaxy/weather/DayWeather.java
@@ -1,9 +1,12 @@
 package com.mercadolibre.galaxy.weather;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class DayWeather {
 
 	private int day;
 	private Weather weather;
+	@JsonIgnore // in order to avoid showing in the HTTP response
 	private double value;
 
 	public DayWeather(int day, Weather weather, double value) {
