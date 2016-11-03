c6bf008a8dc4bf21c0379a2b052cb8133e90c1ad
Emiliano Menendez <emenendez@starmount.com>
2016-11-03 14:07:56 -0300

    Modified WeatherQueryController to retrieve precalculated weather using DayWeatherRepository

--- src/main/java/com/mercadolibre/galaxy/weather/report/WeatherQueryController.java
+++ src/main/java/com/mercadolibre/galaxy/weather/report/WeatherQueryController.java
@@ -7,15 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mercadolibre.galaxy.SolarSystem;
 import com.mercadolibre.galaxy.weather.DayWeather;
+import com.mercadolibre.galaxy.weather.persistence.DayWeatherRepository;
 
 @RestController
 public class WeatherQueryController {
 
 	@Autowired
+	private DayWeatherRepository weatherRepository;
+	
+	@Autowired
 	private SolarSystem solarSystem;
 	
 	@RequestMapping("/clima")
 	public DayWeather getWeatherForDay(@RequestParam(name="dia", required=true) int day) {
-		return solarSystem.getWeatherForDay(day);
+		DayWeather dayWeather = weatherRepository.findOne(day);
+
+		if (dayWeather == null) {
+			dayWeather = solarSystem.getWeatherForDay(day);
+			weatherRepository.save(dayWeather);
+		}
+		
+		return dayWeather;
 	}
 }
