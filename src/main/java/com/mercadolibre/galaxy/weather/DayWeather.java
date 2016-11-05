08b4576f04df596e2643f5cc26fe21df8d5e09a6
Emiliano Menendez <emenendez@starmount.com>
2016-11-05 18:42:07 -0300

    Added WeatherQueryController integration test

--- src/main/java/com/mercadolibre/galaxy/weather/DayWeather.java
+++ src/main/java/com/mercadolibre/galaxy/weather/DayWeather.java
@@ -58,6 +58,28 @@ public class DayWeather {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + day;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DayWeather other = (DayWeather) obj;
+		if (day != other.day)
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("DayWeather [day=");
--- src/main/java/com/mercadolibre/galaxy/weather/Weather.java
+++ src/main/java/com/mercadolibre/galaxy/weather/Weather.java
@@ -11,6 +11,7 @@ public enum Weather {
 		this.value = value;
 	}
 
+	// see http://www.baeldung.com/jackson-serialize-enums
 	@JsonValue
 	public String getValue() {
 		return value;
